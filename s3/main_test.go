f9caab3b18075a43d08dc2f9e6ff4a992a5640ea
minhtri06 <pmtri.tvv@gmail.com>
2025-07-08 23:47:28 +0700

    introduce s3 as a little improved version of s2

---
+++ s3/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	writeresult "github.com/minhtri06/1brc/write_result"
+)
+
+func TestAggregate(t *testing.T) {
+	inputFile := "../measurements_small.txt"
+	expectedOutputFile := "../output_small.txt"
+
+	agg, err := aggregate(inputFile)
+	if err != nil {
+		t.Fatalf("aggregate failed: %v", err)
+	}
+
+	// Convert agg to map[string]string for writeresult.ToFile
+	res := make(map[string]string)
+	for k, v := range agg {
+		res[k] = fmt.Sprintf("%.1f/%.1f/%.1f", v.Min, v.Mean, v.Max)
+	}
+
+	tmpDir := t.TempDir()
+	tmpOutput := filepath.Join(tmpDir, "result.txt")
+	err = writeresult.ToFile(tmpOutput, res)
+	if err != nil {
+		t.Fatalf("writeresult.ToFile failed: %v", err)
+	}
+
+	expected, err := os.ReadFile(expectedOutputFile)
+	if err != nil {
+		t.Fatalf("failed to read expected output: %v", err)
+	}
+	actual, err := os.ReadFile(tmpOutput)
+	if err != nil {
+		t.Fatalf("failed to read actual output: %v", err)
+	}
+
+	if string(expected) != string(actual) {
+		t.Errorf("output mismatch\nExpected:\n%s\nActual:\n%s", expected, actual)
+	}
+}
