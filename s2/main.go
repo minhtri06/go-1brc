1f358c86ec6788c0281704b4ad828fb82c70b97a
minhtri06 <pmtri.tvv@gmail.com>
2025-07-17 00:49:08 +0700

    s2 - refactor

--- s2/main.go
+++ s2/main.go
@@ -5,10 +5,22 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"math"
 	"os"
 	"runtime/pprof"
 )
+
+// This solution is the same as solution 1, but added some optimizations:
+// - It processes station names and values in bytes -> reduce the time for string allocation.
+// - It uses sumX10 (x10), making it an integer -> more accurate than floating point numbers.
+
+type Aggregation struct {
+	min  float64
+	mean float64
+	max  float64
+
+	sumX10 int64
+	count  int64
+}
 
 const inputFile = "../measurements.txt"
 
@@ -28,17 +40,6 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("aggregate failed: %w", err))
 	}
-}
-
-type Aggregation struct {
-	Min  float64
-	Mean float64
-	Max  float64
-
-	MinX10 int
-	MaxX10 int
-	SumX10 int64
-	Count  int64
 }
 
 func aggregate(inputFile string) (map[string]*Aggregation, error) {
@@ -68,66 +69,71 @@ func aggregate(inputFile string) (map[string]*Aggregation, error) {
 		if err != nil {
 			return nil, fmt.Errorf("invalid value at line %v: %w", lineNum, err)
 		}
+		val := float64(valX10) / 10
 
 		a, ok := agg[name]
 		if !ok {
-			a = &Aggregation{
-				MinX10: math.MaxInt,
-				MaxX10: math.MinInt,
+			agg[name] = &Aggregation{
+				min:    val,
+				mean:   val,
+				max:    val,
+				sumX10: valX10,
+				count:  1,
 			}
-			agg[name] = a
+		} else {
+			a.min = min(a.min, val)
+			a.max = max(a.max, val)
+			a.sumX10 += valX10
+			a.count++
 		}
-
-		if valX10 > a.MaxX10 {
-			a.MaxX10 = valX10
-		}
-		if valX10 < a.MinX10 {
-			a.MinX10 = valX10
-		}
-		a.SumX10 += int64(valX10)
-		a.Count++
 	}
-
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("failed to scan file %w", err)
 	}
 
-	for _, stationAgg := range agg {
-		stationAgg.Mean = float64(stationAgg.SumX10) / float64(stationAgg.Count) / 10
-		stationAgg.Min = float64(stationAgg.MinX10) / 10
-		stationAgg.Max = float64(stationAgg.MaxX10) / 10
+	for _, a := range agg {
+		a.mean = float64(a.sumX10) / float64(a.count) / 10
 	}
 
 	return agg, nil
 }
 
-// evaluateValX10 evaluates the value in bytes and return value multiply by 10 to avoid floating point precision issues.
+// evaluateValX10 evaluates the value in bytes and return value multiply by 10 to avoid floating point numbers.
 // E.g. "12.3" will return 123, "-4.5" will return -45.
-func evaluateValX10(valBytes []byte) (int, error) {
-	val := 0
+func evaluateValX10(valBytes []byte) (int64, error) {
+	if len(valBytes) < 3 {
+		return 0, errors.New("value too short")
+	}
+
+	val := int64(0)
 	seenDot := false
 	negative := false
 
-	for _, b := range valBytes {
+	if valBytes[0] == '-' {
+		negative = true
+		valBytes = valBytes[1:]
+	}
+
+	for i, b := range valBytes {
 		if b == '.' {
+			if i != len(valBytes)-2 {
+				return 0, errors.New("invalid dot position")
+			}
 			seenDot = true
-			continue
-		}
-		if b == '-' {
-			negative = true
 			continue
 		}
 		if b < '0' || b > '9' {
 			return 0, errors.New("invalid digit " + string(b))
 		}
-		val = val*10 + int(b-'0')
-		if seenDot {
-			break
-		}
+		val = val*10 + int64(b-'0')
 	}
 
+	if !seenDot {
+		return 0, errors.New("missing dot in value")
+	}
 	if negative {
 		val = -val
 	}
+
 	return val, nil
 }
